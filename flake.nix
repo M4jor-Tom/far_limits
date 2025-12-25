47b5b11cd3c545eae4783a9b9657c9ed1969dfc0
theta <tomvautray@gmail.com>
2025-12-26 00:49:04 +0100

    ci(flake): add dev environment for nixos

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+
+        pkg-config
+
+        # Audio
+        alsa-lib
+
+        # Devices
+        systemd
+
+        # Keyboard / input (X11 + Wayland)
+        libxkbcommon
+
+        # X11
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
+
+        # Wayland (safe to keep even on X11)
+        wayland
+
+        # GPU
+        vulkan-loader
+      ];
+
+      shellHook = ''
+        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+          pkgs.libxkbcommon
+          pkgs.xorg.libX11
+          pkgs.vulkan-loader
+          pkgs.alsa-lib
+        ]}
+      '';
+    };
+  };
+}
